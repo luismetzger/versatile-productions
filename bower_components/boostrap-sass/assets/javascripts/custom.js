b6d45fc03a064fd6571774b3349da7533ac7c546
Luis Metzger <luismetzger@me.com>
2015-09-03 16:46:22 -0600

    Updated navbar and header video on homepage

--- bower_components/boostrap-sass/assets/javascripts/custom.js
+++ bower_components/boostrap-sass/assets/javascripts/custom.js
@@ -1,7 +1,7 @@
-**********************************************************
-  * Toggle tranparent navbar when the user scrolls the page  
-  ********************************************************** 
-  */
+
+//  * Toggle tranparent navbar when the user scrolls the page  
+
+
  $(window).scroll(function() {
     if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
     {
@@ -9,4 +9,56 @@
     } else {
         $('.custom-navbar').removeClass('opaque');
     }
+});
+
+
+//  * Header Video  
+  
+
+var //slide1 = $(".slide1"),
+    slide2 = $(".slide2"),
+    video = $("#video")[0];
+function addSourceToVideo(element, src, type) {    
+    var source = document.createElement('source');    
+    source.src = src;    
+    source.type = type;    
+    element.appendChild(source);    
+}
+addSourceToVideo(video, 'http://cdn.davecampion.com/timelapse-demo/tl3.webm','video/webm');
+addSourceToVideo(video, 'http://cdn.davecampion.com/timelapse-demo/tl3.ogg','video/ogg');
+addSourceToVideo(video, 'http://cdn.davecampion.com/timelapse-demo/tl3.mp4','video/mp4');
+
+
+$('#contact-button').on('click', function(){
+  video.play();
+  
+  slide2.addClass('slideUp');
+  setTimeout(function(){document.getElementById('email').focus();},250);
+  
+});
+
+$('#cancel, .form-close').on('click', function(e){
+   e.preventDefault();
+   slide2.removeClass('slideUp').addClass('slideDownOut');
+   setTimeout(function(){
+     slide2.removeClass('slideDownOut');
+     video.pause();
+   },250); 
+});
+
+$("form").on('submit', function(e){
+	 e.preventDefault();
+  video.playbackRate = 0.5;
+   $('.spinner').removeClass('hidden');
+   $('.sendText').addClass('hidden');
+  $('button').prop('disabled', 'disabled');
+   setTimeout(function(){
+   	  $('.spinner').addClass('hidden');
+      $('.sendText').removeClass('hidden');
+      $('button').prop('disabled', '');
+     video.pause();
+     video.playbackRate = 1;
+     slide2.removeClass('slideUp').addClass('slideLeftOut');
+     setTimeout(function(){slide2.removeClass('slideLeftOut');},250);
+   },2000); 
 });
