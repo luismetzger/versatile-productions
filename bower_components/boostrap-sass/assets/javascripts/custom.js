fdea76b0197219d6db3c782f0495bedde6bf0b5c
Judah Lamm <Judah.Lamm@gmail.com>
2015-09-11 02:18:40 -0600

    videos are displaying properly on mobile, they can be clicked on and played, each time you click on drakes face, the video will play

--- bower_components/boostrap-sass/assets/javascripts/custom.js
+++ bower_components/boostrap-sass/assets/javascripts/custom.js
@@ -1,72 +1,132 @@
 
-//  * Toggle tranparent navbar when the user scrolls the page  
+//  * Toggle tranparent navbar when the user scrolls the page
 
 
- $(window).scroll(function() {
-    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
-    {
-        $('.custom-navbar').addClass('opaque');
-    } else {
-        $('.custom-navbar').removeClass('opaque');
-    }
-});
+//  $(window).scroll(function() {
+//     if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/
+//     {
+//         $('.custom-navbar').addClass('opaque');
+//     } else {
+//         $('.custom-navbar').removeClass('opaque');
+//     }
+// });
 
 
-//  * Header Video  
-  
+//  * Header Video
 
-var //slide1 = $(".slide1"),
-    slide2 = $(".slide2"),
-    video = $("#video")[0];
-function addSourceToVideo(element, src, type) {    
-    var source = document.createElement('source');    
-    source.src = src;    
-    source.type = type;    
-    element.appendChild(source);    
+
+window.onload = function() {
+
+addSourceToVideo('http://cdn.davecampion.com/timelapse-demo/poster.png','http://cdn.davecampion.com/timelapse-demo/tl3.webm','video/webm');
+  var video = $('#videoHouse');
+  var vid = document.getElementById("video");
+  video.click( function (){
+    vid.play();
+  });
+
+
+
+
+};
+  var video1 = $("#video1");
+  var video2 = $("#video2");
+  var video3 = $("#video3");
+
+
+  video1.click( function (){
+    addSourceToVideo('http://cdn.davecampion.com/timelapse-demo/poster.png','http://cdn.davecampion.com/timelapse-demo/tl3.webm','video/webm');
+    var video = $('#videoHouse');
+  var vid = document.getElementById("video");
+  video.click( function (){
+    vid.play();
+  });
+  });
+  video2.click( function () {
+    addSourceToVideo('http://cdn.davecampion.com/timelapse-demo/poster.png', 'http://cdn.davecampion.com/timelapse-demo/tl3.ogg','video/ogg');
+    var video = $('#videoHouse');
+  var vid = document.getElementById("video");
+  video.click( function (){
+    vid.play();
+  });
+  });
+  video3.click( function () {
+    addSourceToVideo('http://cdn.davecampion.com/timelapse-demo/poster.png', 'http://cdn.davecampion.com/timelapse-demo/tl3.mp4','video/mp4');
+    var video = $('#videoHouse');
+  var vid = document.getElementById("video");
+  video.click( function (){
+    vid.play();
+  });
+  });
+
+
+
+
+
+function addSourceToVideo(poster, source, movieType) {
+
+  console.log('clic');
+    var videoContainer = $('.videoContainer');
+
+   $('#video').remove();
+
+    var videoElelment = document.createElement("video");
+    videoElelment.id = "video";
+    videoElelment.src = source;
+    videoElelment.type = movieType;
+    videoElelment.preload = "none";
+    videoElelment.loop = "loop";
+    videoElelment.muted = "muted";
+    videoElelment.volume = 0;
+    videoElelment.poster = poster;
+
+
+    $('.videoContainer').append(videoElelment);
 }
-addSourceToVideo(video, 'http://cdn.davecampion.com/timelapse-demo/tl3.webm','video/webm');
-addSourceToVideo(video, 'http://cdn.davecampion.com/timelapse-demo/tl3.ogg','video/ogg');
-addSourceToVideo(video, 'http://cdn.davecampion.com/timelapse-demo/tl3.mp4','video/mp4');
+
+// addSourceToVideo('http://cdn.davecampion.com/timelapse-demo/tl3.webm','video/webm');
+// addSourceToVideo('http://cdn.davecampion.com/timelapse-demo/tl3.ogg','video/ogg');
+// addSourceToVideo('http://cdn.davecampion.com/timelapse-demo/tl3.mp4','video/mp4');
 
 
-$('#contact-button').on('click', function(){
-  video.play();
-  
-  slide2.addClass('slideUp');
-  setTimeout(function(){document.getElementById('email').focus();},250);
-  
-});
+// $('#startVideoButton').on('click', function(){
+//   console.log('clicked');
+//   video.play();
 
-$('#cancel, .form-close').on('click', function(e){
-   e.preventDefault();
-   slide2.removeClass('slideUp').addClass('slideDownOut');
-   setTimeout(function(){
-     slide2.removeClass('slideDownOut');
-     video.pause();
-   },250); 
-});
+//   slide2.addClass('slideUp');
+//   setTimeout(function(){document.getElementById('email').focus();},250);
 
-$("form").on('submit', function(e){
-	 e.preventDefault();
-  video.playbackRate = 0.5;
-   $('.spinner').removeClass('hidden');
-   $('.sendText').addClass('hidden');
-  $('button').prop('disabled', 'disabled');
-   setTimeout(function(){
-   	  $('.spinner').addClass('hidden');
-      $('.sendText').removeClass('hidden');
-      $('button').prop('disabled', '');
-     video.pause();
-     video.playbackRate = 1;
-     slide2.removeClass('slideUp').addClass('slideLeftOut');
-     setTimeout(function(){slide2.removeClass('slideLeftOut');},250);
-   },2000); 
-});
+// });
 
-// * Mobile Menu Toggle 
+// $('#cancel, .form-close').on('click', function(e){
+//    e.preventDefault();
+//    slide2.removeClass('slideUp').addClass('slideDownOut');
+//    setTimeout(function(){
+//      slide2.removeClass('slideDownOut');
+//      video.pause();
+//    },250);
+// });
 
-$(".menu-toggle").on('click', function() {
-  $(this).toggleClass("on");
-  $('.menu-section').toggleClass("on");
-  $("nav ul").toggleClass('hidden');
-});
+// $("form").on('submit', function(e){
+// 	 e.preventDefault();
+//   video.playbackRate = 0.5;
+//    $('.spinner').removeClass('hidden');
+//    $('.sendText').addClass('hidden');
+//   $('button').prop('disabled', 'disabled');
+//    setTimeout(function(){
+//    	  $('.spinner').addClass('hidden');
+//       $('.sendText').removeClass('hidden');
+//       $('button').prop('disabled', '');
+//      video.pause();
+//      video.playbackRate = 1;
+//      slide2.removeClass('slideUp').addClass('slideLeftOut');
+//      setTimeout(function(){slide2.removeClass('slideLeftOut');},250);
+//    },2000);
+// });
+
+// // * Mobile Menu Toggle
+
+// $(".menu-toggle").on('click', function() {
+//   $(this).toggleClass("on");
+//   $('.menu-section').toggleClass("on");
+//   $("nav ul").toggleClass('hidden');
+// });
