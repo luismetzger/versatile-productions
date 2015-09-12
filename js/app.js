8555165f44b0d100da71523f9f730cf76eec04ba
Eugene Malinin <malinin13@gmail.com>
2015-09-12 14:34:58 +0200

    quotes carousel working prototype

--- js/app.js
+++ js/app.js
@@ -1,16 +1,83 @@
 $(document).ready(function () {
+
+    //Header video
     var iframe = $('#header-player')[0],
         player = $f(iframe);
 
-    $('#header-fullscreen-video-toggle').click(function () {
-        $('.header-fullscreen-video').addClass('header-fullscreen-video-open');
+    var headerVideo = $('.header-fullscreen-video');
+    var headerVideoToggle = $('#header-fullscreen-video-toggle');
+    var headerVideoClose = $('#header-fullscreen-video-close');
+
+    headerVideoToggle.click(function (e) {
+        e.preventDefault();
+        headerVideo.addClass('header-fullscreen-video-open');
         player.api("play");
-    })
+    });
 
-    $('#header-fullscreen-video-close').click(function () {
-        $('.header-fullscreen-video').removeClass('header-fullscreen-video-open');
+    headerVideoClose.click(function (e) {
+        e.preventDefault();
+        headerVideo.removeClass('header-fullscreen-video-open');
         player.api('unload');
+    });
+
+    //Quotes carousel
+    var quotesCarouselVideoIsPlaying = false;
+
+    var quotesCarousel = $('.quotes-carousel');
+    var quotesCarouselItem = $('.quotes-carousel-item');
+    var quotesCarouselItemVideo = quotesCarouselItem.find('iframe')[0];
+
+    var carouselControlItem = $('.quotes-carousel-controls-item');
+
+    var carouselItemVideoToggle = $('.quotes-carousel-item-video-toggle');
+    var carouselItemVideoClose = $('.quotes-carousel-controls-item-video-close');
+
+    quotesCarousel
+        .owlCarousel({
+            //In case of custom animations are needed
+            //animateOut: 'zoomOutDown',
+            //animateIn: 'slideInUp',
+            items: 1
+        })
+        .on('changed.owl.carousel', function (property) {
+            console.log(property.item.index)
+            var current = property.item.index;
+            makeCarouselControlActive(current)
+            quotesCarouselVideoIsPlaying ? carouselCloseVideos() : '';
+        });
+
+    carouselControlItem.click(function () {
+        quotesCarousel.trigger('to.owl.carousel', $(this).attr("data-slide"))
+    });
+
+
+    function makeCarouselControlActive(index) {
+        carouselControlItem.removeClass('active');
+        carouselControlItem.eq(index).addClass('active')
+    }
+
+    function carouselCloseVideos() {
+        console.log('CLOSED ALL')
+        quotesCarouselVideoIsPlaying = false;
+        quotesCarouselItem.removeClass('video-open');
+        $f(quotesCarouselItemVideo).api('unload')
+    }
+
+    carouselItemVideoToggle.click(function (e) {
+        e.preventDefault();
+        var thisParent = $(this).parent();
+        var thisGrandparent = thisParent.parent();
+        thisGrandparent.toggleClass('video-open');
+        $f(thisParent.next()[0]).api('play');
+        quotesCarouselVideoIsPlaying = true;
+    });
+
+    carouselItemVideoClose.click(function (e) {
+        e.preventDefault();
+        var thisParent = $(this).parent();
+        thisParent.toggleClass('video-open');
+        $f(thisParent.find('iframe')[0]).api('unload');
+        quotesCarouselVideoIsPlaying = false;
     })
 
-
-})
+});
